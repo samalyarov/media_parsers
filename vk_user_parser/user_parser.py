6ff811832e739084ed6e2f4ec39a6b2ffbb6e14a
Sam Malyarov <107198574+samalyarov@users.noreply.github.com>
2023-05-31 20:06:44 +0400

    Update user_parser.py
    1. Moved the "users_of_interest" list declaration into the hometown loop - it being declared before the loop (and thus not clearing itself each iteration) caused duplicates in the data.
    2. VK API method user.search only returns first 1000 results regardless of offset (which is used to iterate over posts in the posts parser). In order to circumvent that problem each search needs to return less than 1000 results. Current parser ensures that by looping over hometowns, then - over different ages, then - over different months of birth.

--- vk_user_parser/user_parser.py
+++ vk_user_parser/user_parser.py
@@ -21,12 +21,12 @@ max_user_count = 1000 # How many users to load each cycle. Current VK API limit 
 waiting_time = 1 # Wait time betweeen queries (VK API limits)
 
 # Creating a list of users of interest and a counter for them
-users_of_interest = []
 total_users = 0
 filtered_total_users = 0
 
 # Iterate for each city in a list
 for hometown in monitor_cities.keys():
+    users_of_interest = []
     user_count = vkapi.users.search(hometown=hometown, v=5.131)['count']
     filtered_user_count = vkapi.users.search(hometown=hometown, age_from=20, age_to=25, v=5.131)['count']
 
@@ -36,22 +36,31 @@ for hometown in monitor_cities.keys():
     filtered_total_users += filtered_user_count
 
     # Getting the users:
-    for off in range(0, user_count + 1, max_user_count):
-        print("Loading users beginning with user #{}".format(off))
-        current_users = vkapi.users.search(offset=off, # Current offset
-                                           count=max_user_count, # How many users to return each cycle
-                                           fields=['city', 'last_seen', 'home_town'], # What extra data to return
-                                           age_from=20,
-                                           age_to=25,
-                                           hometown=hometown,
-                                           v=5.131
-                                           )['items'] # Working with the user list
+    for age in range(15, 26, 1):
+        current_users_count = vkapi.users.search(fields=['city', 'last_seen', 'home_town'], # What extra data to return
+                                                 age_from=age,
+                                                 age_to=age,
+                                                 hometown=hometown,
+                                                 v=5.131
+                                                   )['count'] # Working with the user list
+        print(f"Loading users aged: {age}. User count: {current_users_count}")
+
+        for month in range(0, 13, 1):
+            current_users = vkapi.users.search(count=1000, # How many users to return each cycle
+                                               fields=['city', 'last_seen', 'home_town'], # What extra data to return
+                                               age_from=age,
+                                               age_to=age,
+                                               birth_month=month,
+                                               hometown=hometown,
+                                                 v=5.131
+                                                 )['items'] # Working with the user list
         
-        # Adding the users to a list
-        for user in current_users:
-                users_of_interest.append(user)
-        
-        time.sleep(waiting_time)  # Avoiding request limits
+            print(f'Loading users with month of birth set to: {month}. User count: {len(current_users)}')     
+            # Adding the users to a list
+            for user in current_users:
+                    users_of_interest.append(user)
+            
+            time.sleep(waiting_time)  # Avoiding request limits
 
     # Dumpint the data for each town into a separate JSON
     with open(mode = 'a', file=f'users_lists/{hometown}_users.json') as outfile:
